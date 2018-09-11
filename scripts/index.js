d6b02fa4a0c30fca0751876bb51929bd6e40883f
JustinButh <JustinButh91@live.com>
2018-09-11 09:01:54 -0700

    comments

--- scripts/index.js
+++ scripts/index.js
@@ -7,8 +7,13 @@
 
 const UI = (function(){
     const showApp = () => {
+        
+        // add 'display-none' class to #app - loader
         document.querySelector("#app-loader").classList.add('display-none');
+
+        // remove 'hidden' attribute from main
         document.querySelector("main").removeAttribute('hidden');
+
     };
     const loadApp = () => {
         document.querySelector("#app-loader").classList.remove('display-none');
